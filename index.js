72c8876646601e03ae1bc1fe6149e60975cee743
Alexander Ott <contact@alexanderott.at>
2015-09-26 21:45:36 +0200

    finish

--- index.js
+++ index.js
@@ -1,35 +1,54 @@
-var access_token = 'eyJ1c2VyIjoiNTM1NTE5OTE4YzU5Mzc1ZDJiMzM1MzJhIiwiYXBwIjoiNTYwNmUxNDRiMmYyZjYwYzAwNjlhNzNiIiwiY2hhbGxlbmdlIjowLCIkZSI6MH0KEgXDgzfDhMK5LAvDvAdnCEbCm8OFw5w';
-var paymentAccountId = "53565b678c59375d2b339623";
-
-var debitoor = require('debitoor')(access_token);
+// Dependencies
+var debitoor = require('debitoor')(ACCESS_TOKEN);
 var _ = require('underscore');
 var json2csv = require('json2csv');
+var fs = require('fs');
 
-debitoor('/v1.0/paymentaccounts/' + paymentAccountId + '/transactions', function (error, response, body) {
-    var transactions = body;
+// Configuration
+var ACCESS_TOKEN = 'eyJ1c2VyIjoiNTM1NTE5OTE4YzU5Mzc1ZDJiMzM1MzJhIiwiYXBwIjoiNTYwNmUxNDRiMmYyZjYwYzAwNjlhNzNiIiwiY2hhbGxlbmdlIjowLCIkZSI6MH0KEgXDgzfDhMK5LAvDvAdnCEbCm8OFw5w';
+var YEAR = 2014;
 
-    var invoicePayments = [];
+debitoor('/sales/invoices/v1', function(error, response, body){
 
-    _.each(transactions, function(transaction, index, list){
-    	if(transaction.payments) {
-    		_.each(transaction.payments.invoices, function(invoicePayment, index, list){
-	    		invoicePayments.push(invoicePayment);
-	    	});
-    	}
-    });
+	var fieldNames = ['Nr.', 'Rechnungsnummer', 'Rechnungsdatum', 'Buchungsdatum', 'Betrag Netto', 'Betrag Steuer', 'Betrag Brutto'];
+	var fields = ['number', 'invoiceNumber', 'invoiceDate', 'bookingDate', 'amountNet', 'amountTax', 'amountGross'];
 
-    _.each(invoicePayments, function(payment, index, list){
-    	debitoor('/sales/invoices/' + payment.invoiceId + '/v1', function(error, response, body){
+	var data = [];
 
-    		var fields = ['Nr.', 'Rechnungsnummer', 'Rechnungsdatum', 'Buchungsdatum', 'Betrag Netto', 'Betrag Steuer', 'Betrag Brutto']
+	var index = 0;
 
-    		json2csv({ data: myCars, fields: fields }, function(err, csv) {
-			  if (err) console.log(err);
-			  fs.writeFile('file.csv', csv, function(err) {
-			    if (err) throw err;
-			    console.log('file saved');
-			  });
+	_.each(body, function(invoice, i, list){
+		if(invoice.payments){
+			_.each(invoice.payments, function(payment, j, list){
+
+				var paymentDate = new Date(payment.paymentDate);
+
+				if(paymentDate.getYear() + 1900 === YEAR) {
+					var amountGross = payment.amount;
+					var amountNet = amountGross/1.2;
+					var amountTax = amountGross - amountNet;
+
+					data.push({
+						number: index,
+						invoiceNumber: invoice.number,
+						invoiceDate: invoice.date,
+						bookingDate: payment.paymentDate,
+						amountNet: (Math.round(amountNet*100)/100).toString().replace('.', ','),
+						amountTax: (Math.round(amountTax*100)/100).toString().replace('.', ','),
+						amountGross: (Math.round(amountGross*100)/100).toString().replace('.', ',')
+					})
+
+					index++;
+				}
 			});
-    	});
-    });
+		}
+	});
+
+	json2csv({ data: data, fields: fields, fieldNames: fieldNames, del: ';' }, function(err, csv) {
+		if (err) console.log(err);
+			fs.writeFile('income.csv', csv, function(err) {
+		if (err) throw err;
+			console.log('file saved');
+		});
+	});
 });
